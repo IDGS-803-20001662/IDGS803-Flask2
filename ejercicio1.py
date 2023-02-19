7e5a8f34d83cefe77ef049f7e3585b59d2532300
IDGS-803-20001662 <prog.martinez.citlalli@gmail.com>
2023-02-18 20:48:07 -0600

    Correccion Ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -16,23 +16,35 @@ def numeros():
 def numerosresultado():
     num = int(request.form.get("txtNumeroO"))
     lista_num = []
-    # max = 0
-    # min = 0
-    # promedio = 0
-    # suma = 0
+    promedio = 0
+    suma = 0
+    numerosRepetidos = []
+    numeroRepeticiones = []
 
 
     for i in range(num):
-        index = i+1
-        lista_num.append(request.form.get("txtN"+str(index)+'"'))
+        index = "txtN" + str(i+1)
+        print(index)
+        lista_num.append(request.form.get(index))
+
+    max = int(lista_num[0])
+    min = int(lista_num[0])
     
-    #for item in lista_num:
-    #    suma = suma + int(item)
+    for item in lista_num:
+        suma = suma + int(item)
+        if (max <= int(item)):
+            max = int(item)
+        if (min >= int(item)):
+            min = int(item)
+        if lista_num.count(item) > 1:
+            if numerosRepetidos.count(item) == 0 or numerosRepetidos.count(item) == False:
+                numerosRepetidos.append(item)
+                numeroRepeticiones.append(lista_num.count(item))
+                   
+    promedio = suma / len(lista_num)
+    numR = len(numeroRepeticiones)
 
-        
-    #promedio = suma / len(lista_num)
-
-    return render_template("numerosResultado.html", lista_num = lista_num)
+    return render_template("numerosResultado.html", lista_num = lista_num, max = max, min = min, promedio = promedio, numerosRepetidos=numerosRepetidos, numeroRepeticiones=numeroRepeticiones, numR = numR)
     
 
 # A partir de que lugar se comienza la ejecucion
